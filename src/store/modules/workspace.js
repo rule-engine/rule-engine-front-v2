8a3ac31a6a4607487983048d33dd0a10d6416714
laoniu <1603565290m>
2021-06-19 22:03:03 +0800

    update

--- src/store/modules/workspace.js
+++ src/store/modules/workspace.js
@@ -1,20 +1,33 @@
+const storageCurrentWorkSpace = 'storage_currentWorkSpace'
 export default {
     namespaced: true,
     state: {
-        currentWorkSpace: {
-            wsId: 0,
-            name: '默认工作空间'
-        }
+        currentWorkSpace: undefined
     },
     getters: {
         currentWorkSpace: state => {
+            if (!state.currentWorkSpace) {
+                try {
+                    const currentWorkSpace = localStorage.getItem(storageCurrentWorkSpace)
+                    if (!currentWorkSpace) {
+                        state.currentWorkSpace = {
+                            wsId: 0,
+                            name: '默认工作空间'
+                        }
+                    } else {
+                        state.currentWorkSpace = JSON.parse(currentWorkSpace)
+                    }
+                } catch (e) {
+                    console.error(e)
+                }
+            }
             return state.currentWorkSpace
         }
     },
     mutations: {
         setWorkSpace: (state, currentWorkSpace) => {
-            console.log('currentWorkSpace',currentWorkSpace)
             state.currentWorkSpace = currentWorkSpace
+            localStorage.setItem(storageCurrentWorkSpace, JSON.stringify(currentWorkSpace))
         }
     }
 }
