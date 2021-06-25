6177f5757a2dbe7d9c8b8863e3003ccdb706d1f3
dqw <761945125@qq.com>
2021-06-25 18:42:34 +0800

    update

--- src/pages/rulemanagement/generalrule/GeneralRule.vue
+++ src/pages/rulemanagement/generalrule/GeneralRule.vue
@@ -66,7 +66,7 @@
           <a-tag color="cyan" style="padding: 0 4px">预生产(V2.0)</a-tag>
         </div>
 
-        <div slot="action">
+        <div slot="action" slot-scope="{ record}">
           <a style="margin-right: 8px">
             <a-icon type="eye"/>
             查看
@@ -78,20 +78,25 @@
 
           <a-dropdown>
             <a class="ant-dropdown-link" @click="e => e.preventDefault()">
-              更多 <a-icon type="down" />
+              更多
+              <a-icon type="down"/>
             </a>
             <a-menu slot="overlay">
               <a-menu-item>
-                <a-icon type="download" />下载
+                <a-icon type="download"/>
+                下载
+              </a-menu-item>
+              <a-menu-item @click="showHistoryVersion(record)">
+                <a-icon type="history"/>
+                历史版本
+              </a-menu-item>
+              <a-menu-item @click="showAuthority(record)">
+                <a-icon type="team"/>
+                权限
               </a-menu-item>
               <a-menu-item>
-                <a-icon type="history" />历史版本
-              </a-menu-item>
-              <a-menu-item>
-                <a-icon type="team" />权限
-              </a-menu-item>
-              <a-menu-item>
-                <a-icon type="delete" />删除
+                <a-icon type="delete"/>
+                删除
               </a-menu-item>
             </a-menu>
           </a-dropdown>
@@ -102,6 +107,87 @@
         </template>
       </standard-table>
     </a-card>
+
+    <a-modal
+        title="历史版本"
+        :visible="historyVersion.visible"
+        :confirm-loading="historyVersion.confirmLoading"
+        :width="700"
+        @ok="historyVersionHandleOk"
+        @cancel="historyVersionHandleCancel"
+    >
+
+      <standard-table
+          rowKey="id"
+          style="clear: both"
+          :columns="historyVersion.columns"
+          :loading="historyVersion.loading"
+          :dataSource="historyVersion.dataSource"
+          :selectedRows.sync="historyVersion.selectedRows"
+      >
+        <div slot="user" slot-scope="{text, record}">
+          <a-avatar size="small" icon="user" :src="record.avatar"/>
+          {{ record.username }}
+        </div>
+        <div slot="action">
+          <a style="margin-right: 8px">
+            <a-icon type="eye"/>
+            查看
+          </a>
+          <a style="margin-right: 8px">
+            <a-icon type="redo"/>
+            回退
+          </a>
+          <a style="margin-right: 8px">
+            <a-icon type="delete"/>
+            删除
+          </a>
+        </div>
+      </standard-table>
+    </a-modal>
+
+    <a-modal
+        title="权限设置"
+        :visible="authority.visible"
+        :confirm-loading="authority.confirmLoading"
+        :width="700"
+        @ok="authorityVersionHandleOk"
+        @cancel="authorityVersionHandleCancel"
+    >
+
+      <a-form :form="form" layout="inline">
+        <a-form-item label="用户名称/编码">
+          <a-input/>
+        </a-form-item>
+        <a-form-item>
+          <a-button type="primary" @click="queryMember()">
+            搜索
+          </a-button>
+        </a-form-item>
+      </a-form>
+      <br>
+
+      <standard-table
+          rowKey="id"
+          style="clear: both"
+          :columns="authority.columns"
+          :loading="authority.loading"
+          :dataSource="authority.dataSource"
+          :selectedRows.sync="authority.selectedRows"
+      >
+        <div slot="user" slot-scope="{text, record}">
+          <a-avatar size="small" icon="user" :src="record.avatar"/>
+          {{ record.username }}
+        </div>
+        <div slot="edit">
+          <a-switch default-checked/>
+        </div>
+        <div slot="publish">
+          <a-switch/>
+        </div>
+      </standard-table>
+    </a-modal>
+
   </page-layout>
 </template>
 
@@ -119,23 +205,16 @@ const columns = [
     title: '名称',
     width: '180px',
     dataIndex: 'name'
-    // dataIndex: 'user',avatar
-    //scopedSlots: {customRender: 'user'}
-    // scopedSlots: { customRender: 'username' }
   },
   {
     title: '编码',
     dataIndex: 'code',
     width: '180px',
-    // needTotal: true,
-    // customRender: (text) => text + ' 次'
   },
   {
     title: '规则版本状态',
     width: '280px',
     scopedSlots: {customRender: 'versionStatus'}
-    // needTotal: true,
-    // customRender: (text) => text + ' 次'
   },
   {
     title: '创建人',
@@ -164,6 +243,99 @@ export default {
       form: this.$form.createForm(this),
       columns: columns,
       selectedRows: [],
+      authority: {
+        visible: false,
+        confirmLoading: false,
+        loading: false,
+        selectedRows: [],
+        dataSource: [
+          {
+            id: 1,
+            username: "admin",
+            email: "123123@qq.com",
+            avatar: "",
+            createTime: "2020-02-23 9:09:0",
+          }
+        ],
+        columns: [
+          {
+            title: '编号',
+            width: '100px',
+            dataIndex: 'id'
+          },
+          {
+            title: '用户',
+            width: '180px',
+            scopedSlots: {customRender: 'user'}
+          },
+          {
+            title: '邮箱',
+            dataIndex: 'email',
+            width: '180px',
+          },
+          {
+            title: '编辑权限',
+            width: '180px',
+            scopedSlots: {customRender: 'edit'}
+          },
+          {
+            title: '发布权限',
+            width: '180px',
+            scopedSlots: {customRender: 'publish'}
+          },
+        ]
+      },
+      historyVersion: {
+        visible: false,
+        confirmLoading: false,
+        selectedRows: [],
+        loading: false,
+        dataSource: [
+          {
+            id: 1,
+            name: "1",
+            code: "1",
+            version: "v1.0",
+            username: "admin",
+            avatar: "",
+            createTime: "2020-02-23 9:09:0",
+          }
+        ],
+        columns: [
+          {
+            title: '名称',
+            width: '180px',
+            dataIndex: 'name'
+          },
+          {
+            title: '编码',
+            dataIndex: 'code',
+            width: '180px',
+          },
+          {
+            title: '规则版本',
+            width: '180px',
+            dataIndex: 'version',
+            sorter: true
+          },
+          // {
+          //   title: '创建人',
+          //   width: '140px',
+          //   scopedSlots: {customRender: 'user'}
+          // },
+          {
+            title: '创建时间',
+            dataIndex: 'createTime',
+            width: '180px',
+            sorter: true
+          },
+          {
+            title: '操作',
+            width: '220px',
+            scopedSlots: {customRender: 'action'}
+          }
+        ]
+      },
       dataSource: [
         {
           id: '1',
@@ -209,6 +381,31 @@ export default {
       this.dataSource = this.dataSource.filter(item => item.key !== key)
       //this.selectedRows = this.selectedRows.filter(item => item.key !== key)
     },
+    showHistoryVersion(record) {
+      this.historyVersion.visible = true
+      console.log(record)
+    },
+    historyVersionHandleOk() {
+      this.historyVersion.visible = false
+      this.historyVersion.confirmLoading = false
+    },
+    historyVersionHandleCancel() {
+      this.historyVersion.visible = false
+    },
+    showAuthority(record) {
+      this.authority.visible = true
+      console.log(record)
+    },
+    authorityVersionHandleOk() {
+      this.authority.visible = false
+      this.authority.confirmLoading = false
+    },
+    authorityVersionHandleCancel() {
+      this.authority.visible = false
+    },
+    queryMember() {
+
+    }
   }
 }
 </script>
