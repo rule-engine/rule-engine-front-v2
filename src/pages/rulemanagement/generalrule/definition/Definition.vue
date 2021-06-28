a7833cc4a6c9d238fabf44b1ee9fd78dd297ea41
dqw <761945125@qq.com>
2021-06-28 16:54:33 +0800

    add

--- src/pages/rulemanagement/generalrule/definition/Definition.vue
+++ src/pages/rulemanagement/generalrule/definition/Definition.vue
@@ -1,12 +1,32 @@
 <template>
-  <div>
-    asdfadsfasdf123123
-  </div>
+  <page-layout>
+    <a-card :bordered="false">
+      <a-steps :current="0">
+        <a-step>
+          <!-- <span slot="title">Finished</span> -->
+          <template slot="title">
+            规则定义
+          </template>
+          <span slot="description">This is a description.</span>
+        </a-step>
+        <a-step title="In Progress" sub-title="Left 00:00:08" description="This is a description."/>
+        <a-step title="Waiting" description="This is a description."/>
+      </a-steps>
+    </a-card>
+  </page-layout>
 </template>
 
 <script>
+import PageLayout from "@/layouts/PageLayout";
+
 export default {
-  name: "Definition"
+  name: "Definition",
+  components: [PageLayout],
+  data:{
+    return {
+
+    }
+  }
 }
 </script>
 
