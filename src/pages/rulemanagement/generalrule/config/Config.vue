5c22001678a680c8b1c8f614d4faf5f5a3c48ae8
dqw <761945125@qq.com>
2021-06-29 14:33:53 +0800

    update

--- src/pages/rulemanagement/generalrule/config/Config.vue
+++ src/pages/rulemanagement/generalrule/config/Config.vue
@@ -19,6 +19,31 @@
       <a-button type="primary" @click="previous()" :loading="footer.loading">上一步</a-button>&nbsp;&nbsp;&nbsp;&nbsp;
       <a-button type="primary" @click="nextStep()" :loading="footer.loading">下一步</a-button>
     </footer-tool-bar>
+
+
+    <a-button type="primary" @click="showDrawer">
+      Open
+    </a-button>
+
+    <a-drawer
+        placement="left"
+        :closable="false"
+        :visible="drawer.visible"
+        @close="onClose"
+    >
+
+      <a-tabs default-active-key="1">
+        <a-tab-pane key="1" tab="参数">
+          我是参数列表
+        </a-tab-pane>
+        <a-tab-pane key="2" tab="变量">
+          我是变量列表
+        </a-tab-pane>
+        <a-tab-pane key="3" tab="表达式">
+          我是表达式列表
+        </a-tab-pane>
+      </a-tabs>
+    </a-drawer>
   </div>
 </template>
 
@@ -40,7 +65,10 @@ export default {
         code: null,
         description: null,
       },
-      rules: {}
+      rules: {},
+      drawer: {
+        visible: false
+      }
     }
   },
   methods: {
@@ -49,7 +77,13 @@ export default {
     },
     nextStep() {
       //this.$router.push('/')
-    }
+    },
+    showDrawer() {
+      this.drawer.visible = true;
+    },
+    onClose() {
+      this.drawer.visible = false;
+    },
   }
 }
 </script>
