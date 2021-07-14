7f777c5a234b9b608d2b4a4b737bee6b874820f9
dqw <761945125@qq.com>
2021-07-14 19:24:50 +0800

    add

--- src/pages/rulemanagement/generalrule/router/Router.vue
+++ src/pages/rulemanagement/generalrule/router/Router.vue
@@ -35,10 +35,6 @@ export default {
         console.debug(e)
       }
     }
-    window.addEventListener('popstate', this.cancel, false)
-  },
-  destroyed() {
-    window.removeEventListener('popstate', this.cancel, false);
   },
   watch: {
     // 修改路由参数
@@ -49,13 +45,12 @@ export default {
         query: {pageIndex: val}
       })
     },
+    '$route.query.pageIndex'(val) {
+      if (val && !isNaN(val))
+        this.currentPage = parseInt(val)
+    },
   },
   methods: {
-    cancel(data) {
-      var pageIndex = this.$route.query.pageIndex
-      if (data && pageIndex && !isNaN(pageIndex))
-        this.currentPage = parseInt(pageIndex)
-    },
     choicePage(params) {
       this.currentPage = params.pageIndex
       this.ruleId = params.id
