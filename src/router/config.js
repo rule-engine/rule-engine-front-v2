6cdd78a2d23306c5ccd1e47b95631e5f87dd0293
dqw <761945125@qq.com>
2021-07-15 19:45:24 +0800

    代码优化

--- src/router/config.js
+++ src/router/config.js
@@ -24,7 +24,7 @@ const options = {
             path: '/',
             name: '首页',
             component: TabsView,
-            redirect: '/login',
+            redirect: '/dashboard/workplace',
             children: [
                 {
                     path: 'dashboard',
